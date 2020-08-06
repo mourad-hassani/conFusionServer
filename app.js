82129a53b2216368dcd80b5feaa57c6bd1dd05ec
mourad-hassani <im_hassani@esi.dz>
2020-08-06 14:45:57 +0100

    Express Session Part 2

--- app.js
+++ app.js
@@ -41,37 +41,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.signedCookies);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    } else {
-      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      var username = auth[0];
-      var password = auth[1];
-      if (username === 'admin' && password === 'password') {
-        req.session.user = 'admin';
-        next();
-      } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-      }
-    }
+    var err = new Error('You are not authenticated!');
+    err.status = 401;
+    next(err);
   } else {
-    console.log(req.session);
-    if (req.session.user === "admin") {
+    if (req.session.user === "authenticated") {
       next();
     } else {
       var err = new Error('You are not authenticated!');
-      err.status = 401;
+      err.status = 403;
       next(err);
     }
   }
@@ -79,8 +64,7 @@ function auth(req, res, next) {
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
