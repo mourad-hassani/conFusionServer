f0145e54f85a065684b5f677a18959e6b9a8e82f
mourad-hassani <im_hassani@esi.dz>
2020-08-10 13:19:50 +0100

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
 
@@ -41,24 +43,19 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(authenticate.initialize());
+app.use(authenticate.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
-
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 401;
     next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      next(err);
-    }
+    next();
   }
 }
 app.use(auth);
