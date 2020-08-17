9483d7a9d8c244761f462e7036aba4bd2ecc0bd9
mourad-hassani <im_hassani@esi.dz>
2020-08-17 22:39:54 +0100

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '606016723367803',
+        clientSecret: '2edd5d2c6a1db4b2a36721f23e41c6c4'
+    }
 };
